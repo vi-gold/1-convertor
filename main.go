df22caf7483073a43f2da22077d26db97ce0db0b
vi-gold <popovpavelwork@gmail.com>
2025-11-14 14:33:57 +0500

    Implements second homework

--- main.go
+++ main.go
@@ -6,5 +6,22 @@ func main() {
 	const usdToEur float64 = 0.86
 	const usdToRub float64 = 80.87
 	const EurToRub float64 = usdToRub / usdToEur
-	fmt.Print(EurToRub)
+	fmt.Println(EurToRub)
+
+	originCurrency, targetCurrency, sum := getUserInput()
+	converter(originCurrency, targetCurrency, sum)
+}
+
+func getUserInput() (originCurrency, targetCurrency string, sum float64) {
+	fmt.Print("Введите исходную валюту: ")
+	fmt.Scan(&originCurrency)
+	fmt.Print("Введите сумму перевода: ")
+	fmt.Scan(&sum)
+	fmt.Print("Введите целевую валюту: ")
+	fmt.Scan(&targetCurrency)
+	return originCurrency, targetCurrency, sum
+}
+
+func converter(originCurrency, targetCurrency string, sum float64) float64 {
+	return 0
 }
