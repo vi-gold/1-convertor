9fa4a5d3f719c49f112ec8f52c465790b4e88032
vi-gold <popovpavelwork@gmail.com>
2025-12-03 23:24:47 +0500

    Done package

--- 3-bin/main.go
+++ 3-bin/main.go
@@ -1,52 +1,26 @@
 package main
 
 import (
-	"errors"
+	"demo/bin/bins"
 	"fmt"
-	"time"
 )
-
-type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
-}
-
-func (newBin *Bin) outputBin() {
-	fmt.Println(newBin.id, newBin.private, newBin.createdAt, newBin.name)
-}
-
-func (newBin *Bin) outputBinList(binList []Bin) {
-	fmt.Println(binList)
-}
-
-func newBin(id string, private bool, name string) (*Bin, error) {
-	if id == "" || name == "" {
-		return nil, errors.New("EMPTY_ID_OR_NAME")
-	}
-	newBin := &Bin{
-		id:        id,
-		private:   private,
-		createdAt: time.Now(),
-		name:      name,
-	}
-	return newBin, nil
-}
 
 func main() {
 	id := "123"
 	private := true
 	name := "Test"
 
-	myBin, err := newBin(id, private, name)
+	myBin, err := bins.NewBin(id, private, name)
 	if err != nil {
 		fmt.Println("Неверный формат id или Name")
 		return
 	}
-	myBin.outputBin()
-	binList := []Bin{}
-	binList = append(binList, *myBin)
-	myBin.outputBinList(binList)
+	myBin.OutputBin()
+
+	binList := bins.NewBinList()
+
+	binList.AddBin(*myBin)
+
+	binList.OutputBinList()
 
 }
